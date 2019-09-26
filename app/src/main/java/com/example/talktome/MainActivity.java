6f487f24d510f7dbf2174860e253f106df551bf6
yakuphanbilgic <yakuphanbilgic@gmail.com>
2019-09-26 22:12:05 +0300

    add first menu and on utterance listener

--- app/src/main/java/com/example/talktome/MainActivity.java
+++ app/src/main/java/com/example/talktome/MainActivity.java
@@ -5,23 +5,26 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.speech.tts.TextToSpeech;
 import android.speech.RecognizerIntent;
+import android.speech.tts.UtteranceProgressListener;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
+    private TextToSpeech mTTS;
+    private TextView mTextTv;
+    private ImageButton mVoiceBtn;
+    private String speechText;
 
-    private static final int REQUEST_CODE_SPEECH_INPUT=1000;
-
-    //views from activity
-    TextView mTextTv;
-    ImageButton mVoiceBtn;
-
+    private static final int REQUEST_CODE_SPEECH_INPUT = 1000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,36 +34,66 @@ public class MainActivity extends AppCompatActivity {
         mTextTv = findViewById(R.id.textTv);
         mVoiceBtn = findViewById(R.id.voiceBtn);
 
-        //button click to show speech to text dialog
-        mVoiceBtn.setOnClickListener(new View.OnClickListener(){
+        mTTS = new TextToSpeech(this, new TextToSpeech.OnInitListener() {
             @Override
-                    public void onClick(View v){
-                        speak();
+            public void onInit(int status) {
+                if (status == TextToSpeech.SUCCESS) {
+                    int result = mTTS.setLanguage(Locale.UK);
+                    //I skipped the log in part.
+                    ttsInitialized();
+                    if (result == TextToSpeech.LANG_MISSING_DATA
+                            || result == TextToSpeech.LANG_NOT_SUPPORTED) {
+                        Log.e("TTS", "Language not supported");
+                    }
+                } else {
+                    Log.e("TTS", "Initialization failed");
+                }
+            }
+        });
+
+    }
+
+    private void ttsInitialized(){
+        mTTS.setOnUtteranceProgressListener(new UtteranceProgressListener() {
+            @Override
+            public void onStart(String s) {
 
             }
 
+            @Override
+            public void onDone(String s) {
+                listen();
+            }
+
+            @Override
+            public void onError(String s) {
+
+            }
         });
+
+        speechText = "Welcome to email client. Menu options are inbox, sent email and compose an email.";
+
+        HashMap<String, String> map = new HashMap<>();
+        map.put(TextToSpeech.Engine.KEY_PARAM_UTTERANCE_ID, "messageID");
+
+        mTTS.speak(speechText, TextToSpeech.QUEUE_FLUSH, null, "messageID");
     }
 
-    private void speak(){
-    //intent to show speech to text dialog
+    private void listen(){
+        //intent to show speech to text dialog
         Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
         intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
-        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE,"en");
         intent.putExtra(RecognizerIntent.EXTRA_PROMPT, "Hi speak something");
 
         //start intent
-
         try{
             //at this block we do not have an error
             startActivityForResult(intent, REQUEST_CODE_SPEECH_INPUT);
-
         }
         catch (Exception e){
-
             //we get the message error if it was one
             Toast.makeText(this, ""+e.getMessage(),Toast.LENGTH_SHORT).show();
-
         }
     }
    // protected void onActivityResult(int requestCode, int ResultCode, @Nullable Intent data){
@@ -77,12 +110,17 @@ public class MainActivity extends AppCompatActivity {
                 //get text array from voice intent
                 if (resultCode == RESULT_OK && null != data){
                     ArrayList<String> result= data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
-                    //set to text view
-                    mTextTv.setText(result.get(0));
+
+                    //
+                    // this is where menu speech ends.
+                    // write switch case for each menu item
+                    // and start the activity for respective menu option
+                    //
+                    // speak(result.get(0));
+                    //
                 }
                 break;
             }
-
         }
     }
 }
